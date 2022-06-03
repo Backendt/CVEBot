87c3d2d11308353b94ee651f4c24600a55611f55
Backendt <matisse.beckandt@gmail.com>
2022-06-03 02:47:55 +0200

    Initial Commit

---
+++ src/main/java/fr/backendt/cvebot/CveBotApplication.java
@@ -0,0 +1,13 @@
+package fr.backendt.cvebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CveBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CveBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/backendt/cvebot/CveBotApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.backendt.cvebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CveBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
